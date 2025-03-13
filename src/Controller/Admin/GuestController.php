362c4d0721bed046f7e98f66a9accdf8194a7506
Loulou <shiploulou.dev@gmail.com>
2025-03-13 15:27:55 +0100

    Création de la page pour ajouter un utilisateur

--- src/Controller/Admin/GuestController.php
+++ src/Controller/Admin/GuestController.php
@@ -2,18 +2,23 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\Query\Expr\Func;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class GuestController extends AbstractController
 {
     public function __construct(
         private UserRepository $userRepository,
-        private EntityManagerInterface $em
+        private EntityManagerInterface $em,
+        private UserPasswordHasherInterface $encoder
     ) {}
 
     #[Route('/admin/guest', name: 'admin_guest_index')]
@@ -37,5 +42,29 @@ class GuestController extends AbstractController
             'total' => $total,
             'page' => $page
         ]);
+    }
+
+    #[Route('/admin/guest/add', name: 'admin_guest_add')]
+    public function add(Request $request): Response
+    {
+        $form = $this->createForm(UserType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var User */
+            $user = $form->getData();
+
+            $password = $form->getData()->getPassword();
+            $hash = $this->encoder->hashPassword($user, $password);
+
+            $user->setPassword($hash);
+
+            $this->em->persist($user);
+            $this->em->flush();
+
+            return $this->redirectToRoute('admin_guest_index');
+        }
+
+        return $this->render('admin/guest/add.html.twig', ['form' => $form->createView()]);
     }
 }
