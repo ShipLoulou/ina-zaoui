dae8c0de31e4c6a81397fa06140019e58209efb4
Loulou <shiploulou.dev@gmail.com>
2025-03-21 13:53:01 +0100

    Création LoginTest

---
+++ tests/Functional/Auth/LoginTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Auth;
+
+use App\Tests\Functional\FunctionalTestCase;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+class LoginTest extends FunctionalTestCase
+{
+    public function testThatLoginShouldSucceeded(): void
+    {
+        $this->get('/login');
+
+        $this->client->submitForm('Connexion', [
+            '_username' => 'ina@zaoui.com',
+            '_password' => 'password'
+        ]);
+
+        $authorizationChecker = $this->service(AuthorizationCheckerInterface::class);
+
+        self::assertTrue($authorizationChecker->isGranted('IS_AUTHENTICATED'));
+    }
+
+    public function testThatLoginShouldFailed(): void
+    {
+        $this->get('/login');
+
+        $this->client->submitForm('Connexion', [
+            '_username' => 'ina@zaoui.com',
+            '_password' => 'fail'
+        ]);
+
+        $authorizationChecker = $this->service(AuthorizationCheckerInterface::class);
+
+        self::assertFalse($authorizationChecker->isGranted('IS_AUTHENTICATED'));
+    }
+}
